9df696235f951bfc80e545d6ca90314866cbed85
Wei Yuxin <weiyuxin100@gmail.com>
2018-07-16 20:56:44 +0800

    Added Login Screen

--- app/src/main/java/com/example/weiyushit/eventureinjava/Login_Screen.java
+++ app/src/main/java/com/example/weiyushit/eventureinjava/Login_Screen.java
@@ -3,12 +3,11 @@ package com.example.weiyushit.eventureinjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login_Screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login__screen);
     }
 }
-
