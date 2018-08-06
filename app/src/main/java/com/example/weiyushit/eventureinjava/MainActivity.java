7ef36b1cc0e69c5d25b6bfc1554dd439b4d31d1b
Wei Yuxin <weiyuxin100@gmail.com>
2018-08-06 08:58:42 +0800

    addition of missing lines like "android.widget.___"

--- app/src/main/java/com/example/weiyushit/eventureinjava/MainActivity.java
+++ app/src/main/java/com/example/weiyushit/eventureinjava/MainActivity.java
@@ -2,8 +2,25 @@ package com.example.weiyushit.eventureinjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+
+        Log.i("Info","Clicked!");
+
+
+    }
+
+
+    public void signInFunction(View button){
+        EditText EditTextPassword = findViewById('button2');
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
